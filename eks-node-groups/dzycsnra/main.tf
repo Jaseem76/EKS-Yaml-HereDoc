f378cf5b9cff6a07dad5df83de620330dd8c5ef7
Jaseem76 <jaseem.p@sedai.io>
2026-09-08 14:03:20 +0530

    Update main.tf

--- eks-node-groups/dzycsnra/main.tf
+++ eks-node-groups/dzycsnra/main.tf
@@ -41,6 +41,9 @@ locals {
     selfManagedNodeGroup:
       nodeGroupName: sedai-labs-beta-smng
       minSize: 0
+      desiredSize: 0
+      maxSize: 0
+      instanceTypes: [t3.medium]
       diskSize: 20
       launchTemplate:
         name: sedai-labs-beta-smng-2026052211254333540000000b
@@ -114,8 +117,8 @@ module "self_managed_node_group" {
   subnet_ids      = local.config.subnetIds
 
   min_size        = local.config.selfManagedNodeGroup.minSize
-  desired_size    = try(local.config.selfManagedNodeGroup.desiredSize,null)
-  max_size        = try(local.config.selfManagedNodeGroup.maxSize,null)
+  desired_size    = local.config.selfManagedNodeGroup.desiredSize
+  max_size        = local.config.selfManagedNodeGroup.maxSize
   launch_template = local.config.selfManagedNodeGroup.launchTemplate
   instance_types  = try(local.config.selfManagedNodeGroup.instanceTypes, null)
   disk_size       = try(local.config.selfManagedNodeGroup.diskSize, null)
